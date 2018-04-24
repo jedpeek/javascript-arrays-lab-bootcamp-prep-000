53bd8787c8304fed2ab7d1fba2868bf26dc7d2aa
Jed Peek <jedpeek@yahoo.com>
2018-04-24 20:14:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name){
+  var arr2 = [kittens,... name]
+  return arr2
+}
+
+function prependKitten(name){
+  var arr2 = [... name, kittens]
+  return arr2
+}
